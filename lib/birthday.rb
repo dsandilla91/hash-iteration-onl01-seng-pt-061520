f2bcb30ac4533c82e7ee79e80d32b2ea7e1c4bc8
Deborah Santillan <s.debb@yahoo.com>
2020-06-20 16:31:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -17,7 +17,7 @@ def age_appropriate_birthday(birthday_kids)
     if age <= 12 
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   else age >= 12 
-    puts "#{kids_name}, is #{age}"
+    puts "#{kids_name}, you are too old for this."
   end
 end
 end 
